27f7c0de409f65ee7f9dc9ba434095a207a797ac
Christian Schefe <schefe.c@eppendorf.de>
2024-08-05 11:01:03 +0200

    feat: crop bg instead of stretching it

--- src/app.tsx
+++ src/app.tsx
@@ -5,14 +5,14 @@ import { ProjectCard } from './projectCard';
 function App() {
   return (
     <div className="w-full h-full flex-grow flex flex-col bg-gray-100 dark:bg-neutral-900">
-      <img src="/bg.jpg" className='absolute w-full h-full overflow-hidden'></img>
+      <img src="/bg.jpg" id="bg-image" className="absolute w-full h-full overflow-hidden object-cover"></img>
       <NavBar />
       <div className="w-full h-full flex flex-col items-center">
         <div className="max-w-2xl h-full flex flex-col w-full m-8 gap-6">
           <ProjectCard
             title="Yeast - Your Easy & Awesome Serialization Toolkit"
             description="Yeast is a Unity Code Utility Package for serializing and deserializing data to different formats such as JSON and XML"
-            githubUrl='https://github.com/Christian-Schefe/Yeast'
+            githubUrl="https://github.com/Christian-Schefe/Yeast"
             image="/yeast.png"
           />
         </div>
--- src/navbar.tsx
+++ src/navbar.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from './themeToggle';
 
 export const NavBar = () => {
   return (
-    <GlassCard className="w-full p-2" padding="">
+    <GlassCard className="w-full p-4" padding="">
       <div className="p-2 w-full h-full flex-row flex items-center gap-4">
         <img src="/logo.png" className="h-10 rounded-lg bg-neutral-800"></img>
         <div className="flex flex-row grow gap-2"></div>
